166b32488b3209a2ac3811699241f8c96709e46b
iuliana <iuliana.cosmina@gmail.com>
2018-09-01 23:31:49 +0100

    added a few more packages to export

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,12 @@
  * Created by iuliana.cosmina on 1/9/18.
  */
 module common.sanselan {
-    requires  java.desktop;
+    requires java.desktop;
+    exports org.apache.commons.imaging;
+    exports org.apache.commons.imaging.color;
+    exports org.apache.commons.imaging.common;
+    exports org.apache.commons.imaging.icc;
+    exports org.apache.commons.imaging.palette;
+    exports org.apache.commons.imaging.util;
+    exports org.apache.commons.imaging.formats.jpeg.exif;
 }
